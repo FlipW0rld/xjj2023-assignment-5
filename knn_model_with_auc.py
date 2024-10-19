c9d532738ff90c37b8da37381de7f26c0c36c523
FlipW0rld <144553929+FlipW0rld@users.noreply.github.com>
2024-10-18 21:19:54 -0400

    Update knn_model_with_auc.py

--- knn_model_with_auc.py
+++ knn_model_with_auc.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score ## This library is not used for KNN algorithm
 
 # ------------------- Data Preprocessing -------------------
 
