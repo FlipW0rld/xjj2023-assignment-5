5f5d36c7080356121ba31a909639c057b805fea4
JiangjianXie <jiangjianxie0703@gmail.com>
2024-10-18 21:04:20 -0400

    second commit

--- knn_model_with_auc.py
+++ knn_model_with_auc.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.metrics import roc_auc_score
 
 train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
@@ -30,67 +32,70 @@ means = train_df[numerical_features].mean()
 stds = train_df[numerical_features].std()
 
 train_df[numerical_features] = (train_df[numerical_features] - means) / stds
-
 test_df[numerical_features] = (test_df[numerical_features] - means) / stds
 
-#----------------------------------
-
 test_ids = test_df['id']
-
 
 train_df = train_df.drop(['CustomerId', 'Surname', 'id'], axis=1)
 test_df = test_df.drop(['CustomerId', 'Surname', 'id'], axis=1)
 
-X_train = train_df.drop('Exited', axis=1)
-y_train = train_df['Exited']
-
-X_train = X_train.values
-y_train = y_train.values
+X = train_df.drop('Exited', axis=1).values
+y = train_df['Exited'].values
 X_test = test_df.values
 
-#----------------------------------
-
 def euclidean_distance(x1, x2):
-    """
-    Calculate the Euclidean distance between two vectors.
-    """
     return np.sqrt(np.sum((x1 - x2) ** 2))
 
 def get_neighbors(X_train, y_train, test_instance, k):
-    """
-    Find the k nearest neighbors of the test_instance.
-    """
     distances = []
     for i in range(len(X_train)):
         dist = euclidean_distance(X_train[i], test_instance)
         distances.append((dist, y_train[i]))
-    
     distances.sort(key=lambda x: x[0])
-    
     neighbors = distances[:k]
     return neighbors
 
 def predict_probability(X_train, y_train, test_instance, k):
-    """
-    Predict the probability of 'Exited' for the test_instance.
-    """
     neighbors = get_neighbors(X_train, y_train, test_instance, k)
-    
     output_values = [neighbor[1] for neighbor in neighbors]
     probability = sum(output_values) / k
     return probability
 
-#----------------------------------
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+k_values = range(1, 21)
+mean_auc_scores = []
 
-k = 5  
+for k in k_values:
+    auc_scores = []
+    print(f"Evaluating k={k}")
+    for train_index, val_index in kf.split(X):
+        X_train_cv, X_val_cv = X[train_index], X[val_index]
+        y_train_cv, y_val_cv = y[train_index], y[val_index]
+        
+        y_val_preds = []
+        for index in range(len(X_val_cv)):
+            test_instance = X_val_cv[index]
+            prob = predict_probability(X_train_cv, y_train_cv, test_instance, k)
+            y_val_preds.append(prob)
+        
+        auc = roc_auc_score(y_val_cv, y_val_preds)
+        auc_scores.append(auc)
+    
+    mean_auc = np.mean(auc_scores)
+    mean_auc_scores.append(mean_auc)
+    print(f"k={k}, Mean AUC-ROC: {mean_auc}")
+
+best_k_index = np.argmax(mean_auc_scores)
+best_k = k_values[best_k_index]
+print(f"\nBest k: {best_k} with Mean AUC-ROC: {mean_auc_scores[best_k_index]}")
 
 predictions = []
 
-print("\nStarting predictions...")
+print("\nStarting predictions with best k...")
 
 for index in range(len(X_test)):
     test_instance = X_test[index]
-    prob = predict_probability(X_train, y_train, test_instance, k)
+    prob = predict_probability(X, y, test_instance, best_k)
     sample_id = test_ids.iloc[index]
     predictions.append((sample_id, prob))
     if (index + 1) % 100 == 0 or index == len(X_test) - 1:
@@ -98,10 +103,7 @@ for index in range(len(X_test)):
 
 print("Predictions completed.")
 
-#---------------------------------------
-
 submission_df = pd.DataFrame(predictions, columns=['id', 'Exited'])
-
 submission_df.to_csv('submission.csv', index=False)
 
-print("\nSubmission file 'submission.csv' has been generated.")
+print(f"\nSubmission file 'submission.csv' has been generated using k={best_k}.")
